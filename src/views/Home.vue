596137950621d19eb2540ec9a61380882a89e601
godofjs <henryeze019@gmail.com>
2021-02-09 22:36:06 +0000

    auth page set up

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <form>
+      <input placeholder="Email" />
+      <input placeholder="Name" />
+      <input placeholder="Password" />
+      <button>Register</button>
+    </form>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+  components: {},
+  methods: {
   }
 };
 </script>
+<style lang="scss" scoped>
+@import "../assets/styles/style.scss";
+form {
+  width: 30%;
+  margin: auto;
+  margin-top: 20px;
+  input {
+    width: 95%;
+    outline: none;
+    background-color: white;
+    border: transparent;
+    @include box-shadow();
+    padding: 10px;
+    height: 28px;
+    border-radius: 5px;
+    margin-top: 20px;
+  }
+  button {
+    width: 100%;
+    padding: 10px;
+    height: 45px;
+    border-radius: 5px;
+    margin-top: 20px;
+    outline: none;
+    border: transparent;
+    cursor: pointer;
+  }
+}
+</style>
